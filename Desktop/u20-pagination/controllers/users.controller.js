230ed71bdbc2330b9d0ba6657c24e186085e9335
harvin99 <hungtx1001@gmail.com>
2020-06-02 15:17:02 +0700

    Exercise u20 Pagination

---
+++ Desktop/u20-pagination/controllers/users.controller.js
@@ -0,0 +1,92 @@
+const bcrypt = require('bcrypt')
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const shortid = require("shortid");
+//for db
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+module.exports.getUser = (req, res) => {
+  const user = db
+    .get("users")
+    .find({ id: req.signedCookies.userId })
+    .value();
+
+  var page = parseInt(req.query.page) || 1
+  const perPage = 8
+
+  var start = (page - 1)* perPage
+  var end = page * perPage
+  var items = db.get("users").value().slice(start, end)
+
+  if (user.isAdmin) {
+    res.render("users", {
+      users: items,
+      currenPage: page,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      countUser: db.get("users").value().length,
+      isAdmin: true
+    });
+  }
+  else{
+    res.render("users", {
+      users: user,
+      countUser: 1
+    })
+  }
+};
+module.exports.createUser = (req, res) => {
+  res.render("create_user");
+};
+module.exports.postCreateUser = async (req, res) => {
+  const existEmailUser = db
+    .get("users")
+    .find({ email: req.body.email })
+    .value();
+  if (existEmailUser) {
+    res.render("create_user", {
+      values: req.body,
+      errors: ["Email was exist !"]
+    });
+    return;
+  }
+  const salt = await bcrypt.genSalt()
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  
+  const user = {
+    id: shortid.generate(),
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: hashedPassword,
+    isAdmin: false,
+    wrongLoginCount: 0
+  };
+  db.get("users")
+    .push(user)
+    .write();
+  res.redirect("/users");
+};
+
+module.exports.getUserId = (req, res) => {
+  const user = db
+    .get("users")
+    .find({ id: req.params.id })
+    .value();
+  res.render("edit_user", { user: user });
+};
+
+module.exports.postUserId = (req, res) => {
+  db.get("users")
+    .find({ id: req.params.id })
+    .assign({ name: req.body.name, phone: req.body.phone })
+    .write();
+  res.redirect("/users");
+};
+module.exports.getUserIdToDelete = (req, res) => {
+  db.get("users")
+    .remove({ id: req.params.id })
+    .write();
+  res.redirect("/users");
+};
---
+++ Desktop/u20-pagination/routes/users.router.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const userControllers = require('../controllers/users.controller')
+const userValidate = require('../validate/users.validate')
+
+const authMiddleware = require('../middlewares/auth.middleware')
+
+router.get('/', userControllers.getUser)
+router.get('/create_user', userControllers.createUser)
+router.post('/create_user', userValidate.validatePostCreateUser , userControllers.postCreateUser)
+router.get('/:id', userControllers.getUserId)
+router.post('/:id', userControllers.postUserId)
+router.get('/:id/delete', userControllers.getUserIdToDelete)
+
+module.exports = router
